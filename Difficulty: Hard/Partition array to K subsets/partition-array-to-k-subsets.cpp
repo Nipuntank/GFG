14c74cbdaf2de1d3c60664e5b267b8eb6cf3e035
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-08-15 14:25:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Partition array to K subsets/partition-array-to-k-subsets.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution{
+  public:
+    bool solve(int s, int *a, int n, int sum, int k, vector<int> &vis){
+        if(!k)return true; // base case, ki required set sab ban gaye
+        
+        for(int i=0;i<n;i++){
+            if(vis[i])continue;  // agar element visited hai to aage badh jao
+            if(s+a[i]==sum) {  // agar ban gya set to k-1 karke next set dekho
+                vis[i]=1;      // visited kardo is position ko 
+                if(solve(0, a, n, sum, k-1, vis))return true; // sum 0 karke aage jao
+                vis[i]=0;      // backtracking
+            }
+            else if(s + a[i] < sum){  // agar abhi bhi sum nahi bana
+                vis[i]=1;             // to dekhte raho aage 
+                if(solve(s + a[i], a, n, sum, k, vis))return true; 
+                vis[i]=0;  // backtracking
+            }
+        }
+        
+        return false;  
+        
+    }
+    bool isKPartitionPossible(int a[], int n, int k)
+    {
+        int sum=0;
+        for(int i=0;i<n;i++)sum+=a[i];
+        vector<int> arr(n); // ek array maintain karenge, ki is postition wale element 
+                            // ko choose kiya ya nahi
+        if(sum%k)return false;   // agar sum possible hai hi nhi to false return kardo
+        else return solve(0, a, n, sum/k, k, arr);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	
+    	cin>>n;
+    	int a[n];
+    	for(int i=0;i<n;i++)
+    	cin>>a[i];
+    	int k;
+    	cin>>k;
+    	Solution obj;
+    	cout<<obj.isKPartitionPossible(a, n, k)<<endl;
+    }
+}
+// } Driver Code Ends
