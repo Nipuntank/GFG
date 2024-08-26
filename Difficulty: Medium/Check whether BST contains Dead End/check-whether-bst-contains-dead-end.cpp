17c3e03304fa97fbb4cf903695507e8a8203605b
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-08-27 01:53:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check whether BST contains Dead End/check-whether-bst-contains-dead-end.cpp
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+// } Driver Code Ends
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+class Solution{
+  public:
+    int flag=0;
+    int curr=0;
+    int isLeaf=0;
+    int prev=0;
+    void helper(Node *root)
+    {
+        if(root==NULL)
+        {
+            return ;
+        }
+        helper(root->left);
+        if(isLeaf && curr-prev==1 && root->data-curr==1)
+        {
+            flag=1;
+        }
+        else if(isLeaf)
+        {
+            isLeaf=0;
+        }
+        else if(!root->left && !root->right)
+        {
+            isLeaf=1;
+        }
+        prev=curr;
+        curr=root->data;
+        helper(root->right);
+    }
+    bool isDeadEnd(Node *root)
+    {
+        //Your code here
+        helper(root);
+        return flag;
+    }
+};
+
+//{ Driver Code Starts.
+// bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+     Solution ob;
+     cout<<ob.isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
