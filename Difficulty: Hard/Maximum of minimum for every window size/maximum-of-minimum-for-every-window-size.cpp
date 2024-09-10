97eb0e7f57119688326a09f9c294323853901cf0
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-09-10 15:48:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Maximum of minimum for every window size/maximum-of-minimum-for-every-window-size.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find maximum of minimums of every window size.
+    vector <int> maxOfMin(int arr[], int n)
+    {
+        // Your code here
+        vector<int>left(n);
+        vector<int>right(n);
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && arr[st.top()]>=arr[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                left[i]=-1;
+            }
+            else{
+                left[i]=st.top();
+            }
+            st.push(i);
+        }
+        while(!st.empty())
+        {
+            st.pop();
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && arr[st.top()]>=arr[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                right[i]=n;
+            }
+            else{
+                right[i]=st.top();
+            }
+            st.push(i);
+        }
+        vector<int>ans(n+1,0);
+        for(int i=0;i<n;i++)
+        {
+            int len=right[i]-left[i]-1;
+            ans[len]=max(ans[len],arr[i]);
+        }
+        for(int i=n-1;i>=1;i--)
+        {
+            ans[i]=max(ans[i],ans[i+1]);
+        }
+        vector<int>res(n);
+        for(int i=1;i<=n;i++)
+        {
+            res[i-1]=ans[i];
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        vector <int> res = ob.maxOfMin(a, n);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
