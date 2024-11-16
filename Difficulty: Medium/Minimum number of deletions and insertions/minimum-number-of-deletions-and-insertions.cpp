46b1760f60a125ba230182a38fd603f4fd28adf1
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-11-16 17:49:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.cpp
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.cpp
@@ -2,61 +2,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-class Solution{
-		
 
-	public:
-	int helper(int i,int j,string &str1,string &str2,vector<vector<int>>&dp)
-	{
-	    if(i<0 || j<0)
-	    {
-	        return 0;
-	    }
-	    if(dp[i][j]!=-1)
-	    {
-	        return dp[i][j];
-	    }
-	    if(str1[i]==str2[j])
-	    {
-	        return dp[i][j]=1+helper(i-1,j-1,str1,str2,dp);
-	    }
-	    else{
-	        int a=helper(i-1,j,str1,str2,dp);
-	        int b=helper(i,j-1,str1,str2,dp);
-	        return dp[i][j]=max(a,b);
-	    }
-	}
-	int minOperations(string str1, string str2) 
-	{ 
-	    // Your code goes here
-	    int m=str1.size();
-	    int n=str2.size();
-	    vector<vector<int>>dp(m,vector<int>(n,-1));
-	    int a=helper(m-1,n-1,str1,str2,dp);
-	    return m+n-2*a;
-	    
-	} 
+// } Driver Code Ends
+class Solution {
+
+  public:
+    int minOperations(string &s1, string &s2) {
+        // Your code goes here
+        int m=s1.size();
+        int n=s2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return m+n-2*dp[m][n];
+    }
 };
 
 //{ Driver Code Starts.
-int main() 
-{
-   	
-   
-   	int t;
+int main() {
+
+    int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         string s1, s2;
         cin >> s1 >> s2;
 
-	    Solution ob;
-	    cout << ob.minOperations(s1, s2) << "\n";
-	     
+        Solution ob;
+        cout << ob.minOperations(s1, s2) << "\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
-
 
 // } Driver Code Ends
