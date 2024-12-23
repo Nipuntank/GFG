ab83d1478ce831b9783f044ca40e24ddd15e3b15
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-12-23 18:33:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Swaps to Sort/minimum-swaps-to-sort.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to find the minimum number of swaps required to sort the array.
+    int minSwaps(vector<int>& arr) {
+        // Code here
+        vector<pair<int,int>>v;
+        for(int i=0;i<arr.size();i++)
+        {
+            v.push_back({arr[i],i});
+        }
+        sort(v.begin(),v.end());
+        int cnt=0;
+        for(int i=0;i<v.size();)
+        {
+            if(v[i].second==i)
+            {
+                i++;
+                continue;
+            }
+            else{
+                cnt++;
+                swap(v[i],v[v[i].second]);
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        cout << obj.minSwaps(arr) << endl;
+    }
+}
+
+// } Driver Code Ends
