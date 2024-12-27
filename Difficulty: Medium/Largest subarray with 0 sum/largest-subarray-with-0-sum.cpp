a526aa8998a2ebd7bfb9be379bcf80d32e745601
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-12-27 15:12:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -1,33 +1,31 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
 /*You are required to complete this function*/
- 
+
 class Solution {
   public:
-    int maxLen(vector<int>& arr, int n) {
-        // Your code here
-        int maxi=0;
+    int maxLen(vector<int>& arr) {
+        // code here
+        int n=arr.size();
         unordered_map<int,int>mp;
         int sum=0;
+        int ans=0;
         mp[sum]=-1;
+        
         for(int i=0;i<n;i++)
         {
             sum+=arr[i];
             if(mp.find(sum)==mp.end())
             {
-               mp[sum]=i;
+                mp[sum]=i;
             }
-            int a=i-mp[sum];
-            maxi=max(maxi,a);
-            
+            ans=max(ans,i-mp[sum]);
         }
-        return maxi;
+        return ans;
     }
 };
 
@@ -37,16 +35,24 @@ class Solution {
 int main() {
     int t;
     cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
     while (t--) {
-        int m;
-        cin >> m;
-        vector<int> array1(m);
-        for (int i = 0; i < m; ++i) {
-            cin >> array1[i];
-        }
-        Solution ob;
-        cout << ob.maxLen(array1, m) << endl;
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.maxLen(a) << endl;
+        cout << "~\n";
     }
+
     return 0;
 }
 
