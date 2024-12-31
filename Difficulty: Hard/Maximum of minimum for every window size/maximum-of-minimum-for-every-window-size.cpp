5723e17a11f5a53f88cf91a2a3e5f417aa10b9e7
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-12-31 20:16:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Maximum of minimum for every window size/maximum-of-minimum-for-every-window-size.cpp
+++ Difficulty: Hard/Maximum of minimum for every window size/maximum-of-minimum-for-every-window-size.cpp
@@ -5,66 +5,53 @@ using namespace std;
 
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-    //Function to find maximum of minimums of every window size.
-    vector <int> maxOfMin(int arr[], int n)
-    {
+class Solution {
+  public:
+    // Function to find maximum of minimums of every window size.
+    vector<int> maxOfMin(vector<int>& a) {
         // Your code here
-        vector<int>left(n);
-        vector<int>right(n);
-        stack<int>st;
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() && arr[st.top()]>=arr[i])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                left[i]=-1;
-            }
-            else{
-                left[i]=st.top();
-            }
-            st.push(i);
-        }
-        while(!st.empty())
-        {
+        int n=a.size();
+        vector<int>left(n),right(n);
+    stack<int>st;
+    for(int i=0;i<n;i++)
+    {
+        while(!st.empty() && a[st.top()]>=a[i])
             st.pop();
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() && arr[st.top()]>=arr[i])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                right[i]=n;
-            }
-            else{
-                right[i]=st.top();
-            }
-            st.push(i);
-        }
-        vector<int>ans(n+1,0);
-        for(int i=0;i<n;i++)
-        {
-            int len=right[i]-left[i]-1;
-            ans[len]=max(ans[len],arr[i]);
-        }
-        for(int i=n-1;i>=1;i--)
-        {
-            ans[i]=max(ans[i],ans[i+1]);
-        }
-        vector<int>res(n);
-        for(int i=1;i<=n;i++)
-        {
-            res[i-1]=ans[i];
-        }
-        return res;
+        if(st.empty())
+            left[i]=-1;
+        else  
+            left[i]=st.top();
+        st.push(i);
+    }
+    while(!st.empty())
+        st.pop();
+    for(int i=n-1;i>=0;i--)
+    {
+        while(!st.empty() && a[st.top()]>=a[i])
+            st.pop();
+        if(st.empty())
+            right[i]=n;
+        else  
+            right[i]=st.top();
+        st.push(i);
+    }
+    vector<int>ans(n+1,0);
+    for(int i=0;i<n;i++)
+    {
+        int len=right[i]-left[i]-1;
+        ans[len]=max(ans[len],a[i]);
+    }
+    for(int i=n-1;i>=1;i--)
+    {
+        ans[i]=max(ans[i],ans[i+1]);
+    }
+    vector<int>res(n);
+    for(int i=1;i<=n;i++)
+    {
+        res[i-1]=ans[i];
+    }
+    return res;
+
     }
 };
 
@@ -72,16 +59,22 @@ class Solution
 int main() {
     int t;
     cin >> t;
-
+    cin.ignore();
     while (t--) {
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++) cin >> a[i];
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
         Solution ob;
-        vector <int> res = ob.maxOfMin(a, n);
-        for (int i : res) cout << i << " ";
+        vector<int> res = ob.maxOfMin(arr);
+        for (int i : res)
+            cout << i << " ";
         cout << endl;
+        cout << "~\n";
     }
     return 0;
 }
